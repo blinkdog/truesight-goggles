3433f166bd085c15214e444396a83c74ff1c5d50
Patrick Meade <veloxi@ticon.net>
2013-04-16 10:47:03 -0500

    Added persistent configuration state, hyperlinking post URLs
    Loading and saving of configuration state (checkboxes stay un/checked)
    Added a first shot at processing individual posts
    Added a post-handler; naked URLs in posts can be automatically hyperlinked
    Now three tabs: Display, Posts, About

--- src/main/webapp/js/truesight-goggles.user.js
+++ src/main/webapp/js/truesight-goggles.user.js
@@ -18,7 +18,7 @@
 //---------------------------------------------------------------------------
 // ==UserScript==
 // @name            truesight-goggles
-// @version         0.4
+// @version         0.5
 // @updateURL       https://github.com/blinkdog/truesight-goggles/raw/master/src/main/webapp/js/truesight-goggles.user.js
 // @namespace       http://pages.cs.wisc.edu/~meade/greasemonkey/
 // @description     Enhance your experience on Paizo's website
@@ -49,6 +49,115 @@
 // @grant           GM_getResourceText
 // ==/UserScript==
 //---------------------------------------------------------------------------
+
+var linkUnderlineStyles = [
+    "a:link { text-decoration: underline !important; }",
+    "a:visited { text-decoration: underline !important; }",
+    "a:active { text-decoration: underline !important; }",
+    "a:hover { text-decoration: underline !important; }"
+];
+
+//---------------------------------------------------------------------------
+
+var tsgConfig = {};
+
+//---------------------------------------------------------------------------
+
+var applyConfiguration = function() {
+    doLinkUnderline(tsgConfig.checkLinkUnderline);
+    doProcessPosts();
+}
+
+var collateAnchorTags = function(splitHtml) {
+    var result = new Array();
+    var index = 0;
+    while(index < splitHtml.length) {
+        if(isTag(splitHtml[index])) {
+            if(isAnchor(splitHtml[index])) {
+                result.push([
+                    splitHtml[index],
+                    splitHtml[index+1],
+                    splitHtml[index+2]
+                ].join(""));
+                index += 3;
+                continue;
+            }
+        }
+        result.push(splitHtml[index]);
+        index++;
+    }
+    return result;
+}
+
+var doLinkAutolink = function(element) {
+    var index = 0;
+    for(index=0; index<element.splitHTML.length; index++) {
+        if(isTag(element.splitHTML[index]) === false) {
+            element.splitHTML[index] = element.splitHTML[index].replace(
+                /([A-Za-z]+\:\/\/[^<]+)/gm,
+                '<a href="$1" target="_blank">$1</a>');
+        }
+    }
+}
+
+var doLinkUnderline = function(underline) {
+    if(underline) {
+        linkUnderlineStyles.forEach(function(element, index, array) {
+            GM_addStyle(element);
+        });
+    } else {
+        $("style").each(function(index, element) {
+            if(linkUnderlineStyles.indexOf(element.innerHTML) != -1) {
+                $(this).remove();
+            }
+        });
+    }
+}
+
+var doProcessPosts = function() {
+    $(".post itemscope").each(function(index, element) {
+        if(false) {
+            // remove the post instead of working with it
+        }
+        else {
+            // reload the original text of the post
+            element.innerHTML = element.savedInnerHTML;
+            // split the HTML into tags and text
+            element.splitHTML = splitHtml(element.innerHTML);
+            // collate anchor tags into a single array element
+            element.splitHTML = collateAnchorTags(element.splitHTML);
+            // automatically linkify naked URLs
+            if(tsgConfig.checkLinkAutolink) { doLinkAutolink(element); }
+//            // visually mark something
+//            $(this).after("<hr/>");
+            // recombine the split html
+            element.innerHTML = element.splitHTML.join("");
+        }
+    });
+}
+
+var isAnchor = function(text) {
+    return (text[1] === 'a' || text[1] === 'A');
+}
+
+var isTag = function(text) {
+    return (text[0] === '<');
+}
+
+var loadConfiguration = function() {
+    // load the configuration from storage
+    tsgConfig.checkLinkAutolink = (GM_getValue("checkLinkAutolink", "false") === "true");
+    tsgConfig.checkLinkUnderline = (GM_getValue("checkLinkUnderline", "false") === "true");
+    // update the dialog box
+    $("#checkLinkAutolink")[0].checked = tsgConfig.checkLinkAutolink;
+    $("#checkLinkUnderline")[0].checked = tsgConfig.checkLinkUnderline;
+}
+
+var loadPostText = function() {
+    $(".post itemscope").each(function(index, element) {
+        element.innerHTML = element.savedInnerHTML;
+    });
+}
 
 /**
  * Load the jQuery UI CSS and associated image resources.
@@ -108,12 +217,41 @@ var reposition = function() {
     });
 }
 
-var linkUnderlineStyles = [
-    "a:link { text-decoration: underline !important; }",
-    "a:visited { text-decoration: underline !important; }",
-    "a:active { text-decoration: underline !important; }",
-    "a:hover { text-decoration: underline !important; }"
-];
+var saveConfiguration = function() {
+    GM_setValue("checkLinkAutolink", tsgConfig.checkLinkAutolink);
+    GM_setValue("checkLinkUnderline", tsgConfig.checkLinkUnderline);
+}
+
+var savePostText = function() {
+    $(".post itemscope").each(function(index, element) {
+        element.savedInnerHTML = element.innerHTML;
+    });
+}
+
+var splitHtml = function(innerHtml) {
+    var result = new Array();
+    var index = 0;
+    while(index < innerHtml.length) {
+        var nextLeft = innerHtml.indexOf("<", index);
+        if(nextLeft === -1) {
+            var last = innerHtml.substring(index);
+            result.push(last);
+            index = innerHtml.length;
+        } else {
+            var nextRight = innerHtml.indexOf(">", nextLeft+1);
+            if(nextRight === -1) {
+                throw "Unmatched < in innerHtml";
+            } else {
+                var before = innerHtml.substring(index, nextLeft);
+                var tag = innerHtml.substring(nextLeft, nextRight+1);
+                if(before.length > 0) { result.push(before); }
+                result.push(tag);
+                index = nextRight+1;
+            }
+        }
+    }
+    return result;
+}
 
 //---------------------------------------------------------------------------
 // Lights, Camera, Action!
@@ -130,19 +268,23 @@ $(document).ready(function() {
     // create the owl
     $('body').append([
 '<div id="truesight-goggles">',
-    '<img id="truesight-goggles-img" src="http://paizo.com/image/avatar/owl.jpg"/>',
+    '<img id="truesight-goggles-img" src="https://paizo.com/image/avatar/owl.jpg"/>',
 '</div>',
 '<div id="truesight-goggles-dialog" title="another_mage\'s Truesight Goggles">',
     '<div id="truesight-goggles-tabs">',
         '<ul>',
-            '<li><a href="#tab-css">CSS</a></li>',
+            '<li><a href="#tab-display">Display</a></li>',
+            '<li><a href="#tab-posts">Posts</a></li>',
             '<li><a href="#tab-about">About</a></li>',
         '</ul>',
-        '<div id="tab-css">',
-            '<input id="checkLinkUnderline" type="checkbox"/> Underline All Links',
+        '<div id="tab-display">',
+            '<input id="checkLinkUnderline" type="checkbox"/> Underline All Links<br/>',
+        '</div>',
+        '<div id="tab-posts">',
+            '<input id="checkLinkAutolink" type="checkbox"/> Hyperlink URLs<br/>',
         '</div>',
         '<div id="tab-about">',
-            '<p><a href="https://paizo.com/people/another_mage">another_mage</a>\'s Truesight Goggles<br/>License: <a href="http://www.gnu.org/licenses/agpl.html">AGPL v3+</a>&nbsp;Source: <a href="https://github.com/blinkdog/truesight-goggles">GitHub</a></p>',
+            '<p><a href="https://paizo.com/people/anotherMage">another_mage</a>\'s Truesight Goggles<br/>License: <a href="http://www.gnu.org/licenses/agpl.html">AGPL v3+</a>&nbsp;Source: <a href="https://github.com/blinkdog/truesight-goggles">GitHub</a></p>',
             '<p>Thanks to:</p>',
             '<p><a href="https://paizo.com/people/Cheapy">Cheapy</a></p>',
         '</div>',
@@ -163,22 +305,25 @@ $(document).ready(function() {
         $("#truesight-goggles-tabs").tabs();
         $("#truesight-goggles-dialog").dialog();
     });
+    // tell the Hyperlink URLs checkbox how to act
+    $("#checkLinkAutolink").click(function() {
+        tsgConfig.checkLinkAutolink = $("#checkLinkAutolink")[0].checked;
+        saveConfiguration();
+        doProcessPosts();
+    });
     // tell the Underline All Links checkbox how to act
     $("#checkLinkUnderline").click(function() {
-        var checkbox = $("#checkLinkUnderline")[0];
-        if(checkbox.checked) {
-            linkUnderlineStyles.forEach(function(element, index, array) {
-                GM_addStyle(element);
-            });
-        } else {
-            $("style").each(function(index, element) {
-                if(linkUnderlineStyles.indexOf(element.innerHTML) != -1) {
-                    $(this).remove();
-                }
-            });
-        }
+        tsgConfig.checkLinkUnderline = $("#checkLinkUnderline")[0].checked;
+        saveConfiguration();
+        doLinkUnderline(tsgConfig.checkLinkUnderline);
     });
+    // save post text, if any
+    savePostText();
+    // load and apply the configuration
+    loadConfiguration();
+    applyConfiguration();
 });
 
+//debugger;
 //---------------------------------------------------------------------------
 // end of script
